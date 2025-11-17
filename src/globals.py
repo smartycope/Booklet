bb4167bc17da51a4af0b7d63d8d0bfb4a50c5a47
Copeland Carter <smartycope@gmail.com>
2025-11-17 11:06:43 -0600

    sync

--- src/globals.py
+++ src/globals.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 DEBUG = os.uname().nodename == 'zeke'
-ASSETS = Path("../assets").resolve()
+ASSETS = (Path(__file__).parent.parent / "assets").absolute()
 
 WIDTH = 240
 HEIGHT = 240
@@ -12,7 +12,7 @@ print(str((ASSETS / "Orbitron-VariableFont_wght.ttf").absolute()))
 
 _FONT_SIZE = 24
 THEME = {
-    "font": ImageFont.truetype(str((ASSETS / "Orbitron-VariableFont_wght.ttf").absolute()), size=_FONT_SIZE),
+    "font": ImageFont.truetype(str((ASSETS / "Orbitron-VariableFont_wght.ttf")), size=_FONT_SIZE),
     "text_size": _FONT_SIZE,
     "text_color": "#3C2B23",
     "bg": "#FFEFD7"
